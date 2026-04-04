94cb36d0f7c80b8b24f1fdcd485b4a937eb31e73
Mahmud Hasan <2105027@ugrad.cse.buet.ac.bd>
2026-04-05 03:18:16 +0600

    Checking CI/CD 2

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -44,7 +44,7 @@ export function Footer() {
             </ul>
           </div>
           <div>
-            <div className="text-white mb-3 text-[16px]">Checking CI/CD 1</div>
+            <div className="text-white mb-3 text-[16px]">Checking CI/CD 2</div>
           </div>
 
           {/* Connect */}
